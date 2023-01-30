c0631cb32632638fdfb4d7fd02487cba4ae92ed2
павел <@DESKTOP-SO04HOV>
2023-01-30 21:37:39 +0300

    Добавьте файлы проекта.

---
+++ laba1/Money24.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    internal class Money24:Pair
+    {
+        public Money24(double a, double b) : base(a, b)
+        {
+        }
+        public override Pair Add(Pair p)
+        {
+
+            if (this.B + p.B >= 100)
+            {
+                return new Money24(this.A + p.A + 1, (this.B + p.B) - 100);
+            }
+            else return new Money24(this.A + p.A + 1, (this.B + p.B));
+        }
+
+        public override Pair Conj()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Pair Div(Pair p)
+        {
+            return new Money24(this.A / p.A, this.B / p.B);
+        }
+
+        public override bool Equ(Pair p)
+        {
+            if (this.A == p.A && this.B == p.B) return true; 
+            else return false;
+        }
+
+        public override Pair Mul(Pair p)
+        {
+            if (this.B * p.B < 100)
+            {
+                return new Money24(this.A * p.A, this.B * p.B);
+            }
+            else
+            {
+                double temp = this.B * p.B / 100;
+                return new Money24(this.A * p.A + temp, (this.B * p.B) - temp * 100);
+            }
+        }
+
+        public override Pair Sub(Pair p)
+        {
+            if (this.A - p.A >= 0 && this.B - p.B < 0)
+            {
+                return new Money24(this.A - p.A - 1, ((this.B + 100) - p.B));
+            }
+            else if (this.A - p.A < 0)
+            {
+                return new Money24(0, 0);
+            }
+            else return new Money24(this.A + p.A + 1, (this.B + p.B));
+        }
+    }
+}
---
+++ laba1/Pair.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    abstract class Pair
+    {
+        private double a;
+        private double b;
+
+        public double A { get { return a; } set { a = value; } }
+        public double B { get { return b; } set { b = value; } }
+
+        public Pair(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public abstract Pair Add(Pair p);
+        public abstract Pair Sub(Pair p);
+        public abstract Pair Mul(Pair p);
+        public abstract Pair Div(Pair p);
+        public abstract bool Equ(Pair p);
+        public abstract Pair Conj();
+    }
+    
+}
